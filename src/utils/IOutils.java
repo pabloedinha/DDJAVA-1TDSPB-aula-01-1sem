735046e1bd3a269f1aef2abcfa8ac8ae9df94870
pabloedinha <pablooliveiragarcia2015@gmail.com>
2025-09-11 09:46:02 -0300

    Criada a pasta de utilidades para auxiliar na questão de organização.

--- src/utils/IOutils.java
+++ src/utils/IOutils.java
@@ -1,7 +1,15 @@
 package utils; //Criado a pasta 'utils' para organização de código
 
+import java.util.Scanner;
+
 public class IOutils {
     public static void println(String message){
         System.out.println(message);
+    }
+
+    public static int scanInt(Scanner scanner){
+        var input = scanner.nextInt();
+        scanner.nextLine();
+        return input;
     }
 }
