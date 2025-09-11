ad60ae52a84eb1e2d190e94a10837cc67c600fd9
pabloedinha <pablooliveiragarcia2015@gmail.com>
2025-09-11 08:26:12 -0300

    Criação da pasta 'utils' para organização de códigos.

--- src/utils/IOutils.java
+++ src/utils/IOutils.java
@@ -1,4 +1,4 @@
-package utils; //Criado a pasta 'utils' para organização de código.
+package utils; //Criado a pasta 'utils' para organização de código
 
 public class IOutils {
     public static void println(String message){
